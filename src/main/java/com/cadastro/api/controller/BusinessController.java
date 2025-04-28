a72387ceedf1e71a36e1602d55610fcef3987abb
felipematheus1337 <lipehbr@gmail.com>
2025-04-28 19:27:43 -0300

    Finalizado controller.

---
+++ src/main/java/com/cadastro/api/controller/BusinessController.java
@@ -0,0 +1,40 @@
+package com.cadastro.api.controller;
+
+import com.cadastro.api.dtos.PessoaCadastroRequest;
+import com.cadastro.api.dtos.PessoaResponse;
+import com.cadastro.api.services.BusinessService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/v1")
+public class BusinessController {
+
+    private final BusinessService businessService;
+
+    public BusinessController(BusinessService businessService) {
+        this.businessService = businessService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PessoaResponse> cadastrarPessoa(@RequestBody PessoaCadastroRequest request) {
+        PessoaResponse response = businessService.cadastrarPessoa(request);
+
+        URI location = URI.create("/api/v1/" + response.id());
+
+        return ResponseEntity.created(location).body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PessoaResponse> obterPessoaPorId(@PathVariable Long id) {
+        PessoaResponse response = businessService.obterPessoaPorId(id);
+
+        if (response == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+        return ResponseEntity.ok(response);
+
+    }
+}
