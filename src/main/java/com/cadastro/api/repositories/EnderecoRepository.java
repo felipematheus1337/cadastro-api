b7f21b96150c46910a2d6ecce21faaf231926525
felipematheus1337 <lipehbr@gmail.com>
2025-04-28 18:44:43 -0300

    criado repositorios jpa das duas entidades.

---
+++ src/main/java/com/cadastro/api/repositories/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.cadastro.api.repositories;
+
+import com.cadastro.api.model.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
---
+++ src/main/java/com/cadastro/api/repositories/PessoaRepository.java
@@ -0,0 +1,9 @@
+package com.cadastro.api.repositories;
+
+import com.cadastro.api.model.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+}
