bf135483a20a7f1ef504978754d352f01fd85166
felipematheus1337 <lipehbr@gmail.com>
2025-04-29 13:53:39 -0300

    adicionado exceptions personalizadas para @Valid, JSON inválido e parametros ausentes.

--- src/main/java/com/cadastro/api/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/cadastro/api/exceptions/GlobalExceptionHandler.java
@@ -1,60 +1,95 @@
 package com.cadastro.api.exceptions;
 
 import com.cadastro.api.dtos.GenericErrorResponse;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+        String message = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(err -> err.getField() + ": " + err.getDefaultMessage())
+                .findFirst() // mostra só o primeiro erro
+                .orElse("Erro de validação");
+
+        var response = new GenericErrorResponse(status.value(), message);
+        return new ResponseEntity<>(response, status);
+    }
+
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+        var response = new GenericErrorResponse(
+                status.value(),
+                "JSON mal formatado ou inválido."
+        );
+        return new ResponseEntity<>(response, status);
+    }
+
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex,
+                                                                          HttpHeaders headers,
+                                                                          HttpStatus status,
+                                                                          WebRequest request) {
+        var response = new GenericErrorResponse(
+                status.value(),
+                "Parâmetro obrigatório ausente: " + ex.getParameterName()
+        );
+        return new ResponseEntity<>(response, status);
+    }
 
     @ExceptionHandler(TermsNotAcceptedException.class)
     public ResponseEntity<ErrorResponse> handleTermsNotAccepted(TermsNotAcceptedException ex) {
-        ErrorResponse error =  new ErrorResponse(
+        ErrorResponse error = new ErrorResponse(
                 HttpStatus.BAD_REQUEST.value(),
-                ex.getMessage()
-        );
-
+                ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
     public ResponseEntity<ErrorResponse> handleEmailAlreadyExists(EmailAlreadyExistsException ex) {
-        ErrorResponse error =  new ErrorResponse(
+        ErrorResponse error = new ErrorResponse(
                 HttpStatus.BAD_REQUEST.value(),
-                ex.getMessage()
-        );
-
+                ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
     @ExceptionHandler(MissingCnpjException.class)
     public ResponseEntity<ErrorResponse> handleCnpjDoesntExists(MissingCnpjException ex) {
-        ErrorResponse error =  new ErrorResponse(
+        ErrorResponse error = new ErrorResponse(
                 HttpStatus.BAD_REQUEST.value(),
-                ex.getMessage()
-        );
-
+                ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
     @ExceptionHandler(MissingCpfException.class)
     public ResponseEntity<ErrorResponse> handleCpfDoesntExists(MissingCpfException ex) {
-        ErrorResponse error =  new ErrorResponse(
+        ErrorResponse error = new ErrorResponse(
                 HttpStatus.BAD_REQUEST.value(),
-                ex.getMessage()
-        );
-
+                ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
-
-
     @ExceptionHandler(Exception.class)
     public ResponseEntity<GenericErrorResponse> handleGenericException(Exception ex) {
-        var error = new GenericErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage());
-
-        return ResponseEntity.status(error.statusCode()).body(error);
+        var error = new GenericErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Erro interno não tratado: " + ex.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
 }
